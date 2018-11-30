9714d908828d317788b6b670375a303d5a4eebe1
lamartire <lamartire@gmail.com>
2018-11-30 11:21:26 +0300

    Auth app form view bootstrap

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <AuthForm/>
+</template>
+
+<script>
+import AuthForm from '@/components/AuthForm';
+
+export default {
+  name: 'App',
+
+  components: {
+    AuthForm,
+  },
+};
+</script>
+
+<style lang="postcss">
+@import '../node_modules/reset.css/reset.css';
+</style>
+
+<style lang="postcss">
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #6d1f96;
+}
+
+html,
+input,
+button,
+select {
+  font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', Helvetica,
+    Arial, sans-serif;
+}
+</style>
+
---
+++ src/components/VButton.vue
@@ -0,0 +1,67 @@
+<template>
+  <button
+    :class="{ button: true, [type]: type, disabled: disabled }"
+    :disabled="disabled"
+    :type="buttonType"
+    @click="emitClick"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'VButton',
+
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+
+    type: {
+      type: String,
+      default: null,
+    },
+
+    submit: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    buttonType() {
+      return this.submit ? 'submit' : 'button';
+    },
+  },
+
+  methods: {
+    emitClick() {
+      this.$emit('click');
+    },
+  },
+};
+</script>
+
+<style lang="postcss">
+.button {
+  height: 45px;
+  padding: 6.5px 15px;
+  border: none;
+  line-height: 1.5;
+  font-size: 20px;
+  font-weight: 600;
+  cursor: pointer;
+
+  &.primary {
+    background: #4b0472;
+    color: #fff;
+  }
+
+  &.disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+}
+</style>
