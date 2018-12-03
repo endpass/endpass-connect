184ddbd1a18afb0575b3ed26dd927b22ad232a64
lamartire <lamartire@gmail.com>
2018-12-03 14:12:29 +0300

    Add script for splitting built artifacts

--- packages/lib/index.js
+++ packages/lib/index.js
@@ -1,1 +1,15 @@
-console.log(1)
+class Connect {
+  constructor(url) {
+    this.url = url;
+  }
+
+  status() {
+    console.log(1);
+  }
+
+  auth() {
+    console.log(2);
+  }
+}
+
+export default Connect;
