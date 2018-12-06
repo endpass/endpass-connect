b211c14480e210c0c65e9cdcfa217fb621435553
lamartire <lamartire@gmail.com>
2018-12-06 11:23:03 +0300

    Replace rollup with webpack

--- packages/lib/index.js
+++ packages/lib/index.js
@@ -1,12 +1,28 @@
+import Web3 from 'web3';
+
 import identityService from '../service/identity';
+import { Emmiter, InpageProvider } from '../class';
 
 const HOST_WINDOW_NAME = 'endpass-connect-host';
 const DIALOG_WINDOW_NAME = 'endpass-connect-dialog';
 
 class Connect {
-  constructor({ origin, authUrl }) {
-    this.origin = origin;
+  constructor({ authUrl }) {
     this.authUrl = authUrl;
+    this.emmiter = new Emmiter();
+
+    this.setupEmmiterEvents();
+  }
+
+  setupEmmiterEvents() {
+    this.emmiter.on(
+      InpageProvider.INPAGE_EVENTS.REQUEST,
+      this.handleRequest.bind(this),
+    );
+    this.emmiter.on(
+      InpageProvider.INPAGE_EVENTS.SETTINGS,
+      this.handleRequest.bind(this),
+    );
   }
 
   /**
@@ -42,6 +58,7 @@ class Connect {
     window.name = HOST_WINDOW_NAME;
     window.open(this.authUrl, DIALOG_WINDOW_NAME, windowFeatures.join(','));
 
+    // Auth popup closing must reject
     return new Promise(resolve => {
       /* eslint-disable-next-line */
       window.addEventListener('message', message => {
@@ -52,6 +69,23 @@ class Connect {
         }
       });
     });
+  }
+
+  /* eslint-disable-next-line */
+  async injectWeb3() {
+    const inpageProvider = new InpageProvider(this.emmiter);
+
+    if (!window.web3) {
+      window.web3 = new Web3();
+      window.web3.setProvider(inpageProvider);
+      console.log('web3 old', window.web3);
+    } else {
+      console.log('web3 new 112');
+    }
+  }
+
+  async handleRequest(request) {
+    console.log('web3 request', request);
   }
 }
 
