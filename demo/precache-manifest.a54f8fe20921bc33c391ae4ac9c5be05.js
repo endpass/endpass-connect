eca195b21e05e72702a915f144d917bfed157627
lamartire <lamartire@gmail.com>
2019-01-18 18:00:25 +0300

    chore: just add demo and script to deploy it to the gh pages

---
+++ demo/precache-manifest.a54f8fe20921bc33c391ae4ac9c5be05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4115f912676fa0536c9b",
+    "url": "/web3-debugger-dapp/static/js/runtime~main.4115f912.js"
+  },
+  {
+    "revision": "3bd651e3cf0dcd5fcf20",
+    "url": "/web3-debugger-dapp/static/js/main.3bd651e3.chunk.js"
+  },
+  {
+    "revision": "12f9ce85498982ba39bb",
+    "url": "/web3-debugger-dapp/static/js/1.12f9ce85.chunk.js"
+  },
+  {
+    "revision": "3bd651e3cf0dcd5fcf20",
+    "url": "/web3-debugger-dapp/static/css/main.d5403008.chunk.css"
+  },
+  {
+    "revision": "ecb62633281f2a61732bdb75b0965144",
+    "url": "/web3-debugger-dapp/index.html"
+  }
+];
