60a3ba49d6f9008e0770d3ff3637c71c58c2b378
Anton Stepanenkov <Lenseg1@gmail.com>
2019-02-08 21:11:11 +0530

    refactor(env): new keys for google and git

--- env/dev.js
+++ env/dev.js
@@ -5,7 +5,13 @@ const identity = {
   url: 'https://identity-dev.endpass.com',
 };
 
+const googleClientId = '885568627115-2lu9inbk9n7o1hc0me6dip2ac5drprj0.apps.googleusercontent.com';
+
+const gitClientId = '346108128ad51a5eccd1';
+
 module.exports = {
   infura,
-  identity
+  identity,
+  googleClientId,
+  gitClientId
 };
