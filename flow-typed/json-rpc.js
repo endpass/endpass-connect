017d336938385be94f3c5dc363bd3a18918137b6
Alexander Kazimir <alex_kazik@mail.ru>
2019-03-01 15:44:40 +0300

    refactor: add types for rps request/response

--- flow-typed/json-rpc.js
+++ flow-typed/json-rpc.js
@@ -1,4 +1,38 @@
 // @flow
 
-declare type RpcRequest = { [string]: any };
-declare type RpcResponse = { [string]: any };
+declare type RpcVersion = '2.0';
+declare type RpcId = string | number;
+
+declare type RpcRequest = {
+  jsonrpc: RpcVersion,
+  id: RpcId,
+  method: string,
+  params?: Array<any>,
+};
+
+declare type RpcSuccessResponse = {
+  jsonrpc: RpcVersion,
+  id: RpcId,
+  result: any,
+};
+
+declare type RpcErrorResponse = {
+  jsonrpc: RpcVersion,
+  id: RpcId,
+  error: string,
+};
+
+declare type RpcSubscriptionResponse = {
+  jsonrpc: RpcVersion,
+  id: RpcId,
+  method: 'eth_subscription',
+  params: {
+    subscription: string,
+    result: any,
+  },
+};
+
+declare type RpcResponse =
+  | RpcSuccessResponse
+  | RpcErrorResponse
+  | RpcSubscriptionResponse;
