91b886f1b9c7bb7c3060dc390d2ffb09aa7701ca
Alexander Kazimir <alex_kazik@mail.ru>
2019-03-04 19:37:09 +0300

    fix(middleware): check provider id with different types

--- src/middleware/requestProviderCheck.js
+++ src/middleware/requestProviderCheck.js
@@ -3,7 +3,9 @@ import { NET_ID } from '@/constants';
 
 const middleware: Middleware = (context, item) => {
   const { activeNet } = item.settings;
-  const isNetIdAllowed = Object.values(NET_ID).includes(activeNet);
+  const isNetIdAllowed = Object.values(NET_ID)
+    .map(id => String(id))
+    .includes(String(activeNet));
 
   if (isNetIdAllowed) return;
 
