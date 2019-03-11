82d28a5e722f46cb137d4300d6116150e34c66b5
Alex <alex_kazik@mail.ru>
2019-03-11 03:20:18 +0300

    refactor: add async queue

--- src/class/index.js
+++ src/class/index.js
@@ -3,3 +3,5 @@ export { default as InpageProvider } from './InpageProvider';
 export { default as Dialog } from './Dialog';
 export { default as Bridge } from './Bridge';
 export { default as ProviderFactory } from './ProviderFactory';
+export { default as ArrayQueue } from './ArrayQueue';
+export { default as AsyncQueue } from './AsyncQueue';
