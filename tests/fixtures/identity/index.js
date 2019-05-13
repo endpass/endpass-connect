f7c6e675aefc4ef06793a13f7b5df44b10dc348d
Mona Lisa <Lenseg1@gmail.com>
2019-05-13 13:57:59 +0300

    test: identity api fixtures

---
+++ tests/fixtures/identity/index.js
@@ -0,0 +1,70 @@
+//user
+
+export const user = {
+  email: "anton@endpass.com"
+  id: "4da61df9-4e24-4a86-a395-9e1517efb6ed",
+  phones: [{
+      "id": "c4d4ef1c-0d73-4a6f-aad9-7600a8fc79b8",
+    "createdAt": 1557220652,
+    "status": "Verified",
+    "country": "7",
+    "number": "7771112233"
+  }]
+}
+
+
+//accounts
+
+export const accounts = [
+  "0x4Ce2109f8DB1190cd44BC6554E35642214FbE144",
+  "0x166e22d165CE454EDdD21B605481Ce600Cf024BA",
+  "0x4B5a4878DDefc76E77459c9B4514Be81a343471E",
+  "0x6C82209Ad3F45E8115a133F00725bE7FBeDF3B02",
+  "0xe2546ecB10F08852Cf82140186D37BF0EefB667b",
+  "xpub6Dn98Aai134BvGUndk3auCYRkF7cwKWiSyghUpy49NbmPu9pAVrvBtjw3Avw6FF9pmJbFZNE9LB2AzJE3n6JArVwU6hg92umroV3NRvvbFt",
+  "0x4591A72a97b50bba1c526B4e31CcAa5485014f4a"
+];
+
+export const account = {
+  "address": "0x4Ce2109f8DB1190cd44BC6554E35642214FbE144"
+}
+
+//documents
+
+export const documents = [
+  {
+    "id": "f1f80c7a-57c7-4259-8e80-d1bb09932e82",
+    "createdAt": 1543336121,
+    "status": "New",
+    "documentType": "Passport",
+    "description": "Custom document description",
+    "firstName": "Test",
+    "lastName": "User",
+    "number": "47313892501",
+    "dateOfBirth": 1543323121,
+    "dateOfIssue": 1543336122,
+    "dateOfExpiry": 1543336123,
+    "issuingCountry": "Country",
+    "issuingAuthority": "Authority",
+    "issuingPlace": "Place",
+    "address": "Address"
+  }
+]
+
+export const document = {
+  "id": "f1f80c7a-57c7-4259-8e80-d1bb09932e82",
+  "createdAt": 1543336121,
+  "status": "New",
+  "documentType": "Passport",
+  "description": "Custom document description",
+  "firstName": "Test",
+  "lastName": "User",
+  "number": "47313892501",
+  "dateOfBirth": 1543323121,
+  "dateOfIssue": 1543336122,
+  "dateOfExpiry": 1543336123,
+  "issuingCountry": "Country",
+  "issuingAuthority": "Authority",
+  "issuingPlace": "Place",
+  "address": "Address"
+};
