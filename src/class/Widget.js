43ec54f2afea81b08789307eddc193eca301fe36
lamartire <lamartire@gmail.com>
2019-06-28 15:19:49 +0300

    feat(widget): mvp of mobile mode rendering

--- src/class/Widget.js
+++ src/class/Widget.js
@@ -1,31 +1,39 @@
+import debounce from 'lodash.debounce';
 import { METHODS, WIDGET_EVENTS } from '@/constants';
 import { inlineStyles } from '@/util/dom';
 
+const MOBILE_BREAKPOINT = 1024;
 const FADE_TIMEOUT = 300;
-const getStylesByPosition = position => {
-  return (
-    position || {
-      bottom: '15px',
-      right: '15px',
-    }
-  );
+const BASE_FRAME_HEIGHT = 70;
+const BASE_FRAME_STYLES = {
+  position: 'fixed',
+  'z-index': 6000000,
+  height: `${BASE_FRAME_HEIGHT}px`,
+  border: 'none',
+  'border-radius': '4px',
+  transition: `opacity ${FADE_TIMEOUT}ms ease-in`,
 };
-const createWidgetIframeStyles = position => {
-  return inlineStyles(
-    Object.assign(
-      {
-        position: 'fixed',
-        'z-index': 6000000,
-        width: '240px',
-        height: '70px',
-        border: 'none',
-        'border-radius': '4px',
-        opacity: 0,
-        transition: `opacity ${FADE_TIMEOUT}ms ease-in`,
-      },
-      getStylesByPosition(position),
-    ),
-  );
+const INITIAL_FRAME_STYLES = {
+  ...BASE_FRAME_STYLES,
+  opacity: 0,
+};
+const FRAME_DESKTOP_STYLES = {
+  ...BASE_FRAME_STYLES,
+  bottom: '15px',
+  right: '15px',
+};
+const FRAME_MOBILE_STYLES = {
+  ...BASE_FRAME_STYLES,
+  right: '24px',
+  bottom: '24px',
+};
+const FRAME_MOBILE_COLLAPSED_STYLES = {
+  ...FRAME_MOBILE_STYLES,
+  width: '64px',
+};
+const FRAME_MOBILE_EXPANDED_STYLES = {
+  ...FRAME_MOBILE_STYLES,
+  width: 'calc(100% - 24px * 2)',
 };
 
 export default class Widget {
@@ -37,15 +45,43 @@ export default class Widget {
     this.context = context;
     this.url = url;
     this.frame = null;
+    this.position = null;
+
     this.isMounted = false;
+    this.isLoaded = false;
+    this.isExpanded = false;
 
     this.handleWidgetFrameLoad = this.handleWidgetFrameLoad.bind(this);
+    this.handleDocumentClick = this.handleDocumentClick.bind(this);
+    // this.handleScreenResize = this.handleScreenResize.bind(this);
+    // this.debouncedHandleScreenResize = debounce(this.handleScreenResize, 100);
+  }
+
+  /* eslint-disable-next-line */
+  get isMobile() {
+    return window.innerWidth < MOBILE_BREAKPOINT;
   }
 
   subscribe() {
     const widgetMessenger = this.context.getWidgetMessenger();
 
     widgetMessenger.setTarget(this.frame.contentWindow);
+    widgetMessenger.subscribe(METHODS.WIDGET_INIT, (payload, req) => {
+      req.answer({
+        isMobile: this.isMobile,
+      });
+    });
+
+    widgetMessenger.subscribe(METHODS.WIDGET_EXPAND_REQUEST, (payload, req) => {
+      this.isExpanded = true;
+      this.handleDocumentClickOnce();
+      this.frame.style = this.getWidgetFrameInlineStyles();
+      req.answer();
+    });
+    widgetMessenger.subscribe(METHODS.WIDGET_COLLAPSE_REQUEST, () => {
+      this.isExpanded = false;
+      this.frame.style = this.getWidgetFrameInlineStyles();
+    });
 
     widgetMessenger.subscribe(METHODS.WIDGET_OPEN, ({ root }, req) => {
       this.resize({ height: '100vh' });
@@ -62,14 +98,45 @@ export default class Widget {
     });
   }
 
+  handleDocumentClick() {
+    const widgetMessenger = this.context.getWidgetMessenger();
+
+    document.body.removeEventListener('click', this.handleDocumentClick);
+    // this.resize({ width: `${COLLAPSED_MOBILE_FRAME_WIDTH}px` });
+
+    widgetMessenger.send(METHODS.WIDGET_COLLAPSE_RESPONSE);
+  }
+
+  handleDocumentClickOnce() {
+    document.body.addEventListener('click', this.handleDocumentClick);
+  }
+
+  isWidgetMounted() {
+    return this.isMounted;
+  }
+
+  emitFrameEvent(event, detail) {
+    if (!this.frame) {
+      return;
+    }
+
+    const frameEvent = new CustomEvent(event, {
+      detail,
+    });
+
+    this.frame.dispatchEvent(frameEvent);
+  }
+
   /**
    * Create markup and prepend to <body>
    */
   mount(parameters = {}) {
+    const { url } = this;
+
+    this.position = parameters.position || null;
     this.isMounted = true;
 
-    const { url } = this;
-    const styles = createWidgetIframeStyles(parameters.position);
+    const styles = this.getWidgetFrameInlineStyles();
     const markup = `
       <iframe id="endpass-widget" data-endpass="widget-frame" style="${styles}" src="${url}"></iframe>
     `;
@@ -78,6 +145,7 @@ export default class Widget {
 
     this.frame = document.body.querySelector('[data-endpass="widget-frame"]');
     this.frame.addEventListener('load', this.handleWidgetFrameLoad);
+    // window.addEventListener('resize', this.debouncedHandleScreenResize);
 
     this.subscribe();
 
@@ -85,15 +153,53 @@ export default class Widget {
   }
 
   unmount() {
-    this.frame.style.opacity = 0;
+    const widgetMessenger = this.context.getWidgetMessenger();
 
+    this.frame.style.opacity = 0;
+    this.isLoaded = false;
+    this.isMounted = false;
+
+    widgetMessenger.unsubscribe(METHODS.WIDGET_INIT);
+    widgetMessenger.unsubscribe(METHODS.WIDGET_EXPAND_REQUEST);
+    widgetMessenger.unsubscribe(METHODS.WIDGET_OPEN);
+    widgetMessenger.unsubscribe(METHODS.WIDGET_CLOSE);
+    widgetMessenger.unsubscribe(METHODS.WIDGET_FIT);
+
+    this.frame.removeEventListener('load', this.handleWidgetFrameLoad);
+    // window.removeEventListener('resize', this.debouncedHandleScreenResize);
+
+    // Awaiting application animation ending
     setTimeout(() => {
       this.emitFrameEvent(WIDGET_EVENTS.DESTROY);
-      this.frame.removeEventListener('load', this.handleWidgetFrameLoad);
       this.frame.remove();
     }, FADE_TIMEOUT);
+  }
 
-    this.isMounted = false;
+  handleWidgetFrameLoad() {
+    this.emitFrameEvent(WIDGET_EVENTS.MOUNT);
+    this.isLoaded = true;
+
+    console.log('load event', this.getWidgetFrameInlineStyles());
+
+    this.frame.style = this.getWidgetFrameInlineStyles();
+  }
+
+  // handleScreenResize() {
+  //   console.log('resize', this);
+  // }
+
+  /**
+   * @param {String} options.height Height in px or other CSS/HTML friendly unit
+   * @param {String} options.width Width in px or other CSS/HTML friendly unit
+   */
+  resize({ height, width }) {
+    if (height) {
+      this.frame.style.height = height;
+    }
+
+    if (width) {
+      this.frame.style.width = width;
+    }
   }
 
   getWidgetNode() {
@@ -113,31 +219,47 @@ export default class Widget {
     });
   }
 
-  isWidgetMounted() {
-    return this.isMounted;
+  /**
+   * @returns {object}
+   */
+  getWidgetFrameStylesObject() {
+    const { isMobile, isExpanded, isLoaded } = this;
+
+    switch (true) {
+      case isMobile && isLoaded && isExpanded:
+        return {
+          ...FRAME_MOBILE_EXPANDED_STYLES,
+          opacity: Number(this.isLoaded),
+        };
+      case isMobile && isLoaded && !isExpanded:
+        return {
+          ...FRAME_MOBILE_COLLAPSED_STYLES,
+          opacity: Number(this.isLoaded),
+        };
+      case isLoaded:
+        return {
+          ...(this.position || {}),
+          ...FRAME_DESKTOP_STYLES,
+          opacity: Number(this.isLoaded),
+        };
+      default:
+        return INITIAL_FRAME_STYLES;
+    }
   }
 
-  handleWidgetFrameLoad() {
-    this.emitFrameEvent(WIDGET_EVENTS.MOUNT);
-    this.frame.style.opacity = 1;
-  }
+  getWidgetFrameInlineStyles() {
+    const stylesObject = this.getWidgetFrameStylesObject();
 
-  emitFrameEvent(event, detail) {
     if (!this.frame) {
-      return;
+      return inlineStyles(stylesObject);
     }
 
-    const frameEvent = new CustomEvent(event, {
-      detail,
+    const { clientHeight } = this.frame;
+
+    Object.assign(stylesObject, {
+      height: `${clientHeight}px`,
     });
 
-    this.frame.dispatchEvent(frameEvent);
-  }
-
-  /**
-   * @param {String} options.height Height in px or other CSS/HTML friendly unit
-   */
-  resize({ height }) {
-    this.frame.style.height = height;
+    return inlineStyles(stylesObject);
   }
 }
