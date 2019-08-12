2f8cfb2a3d2584d1a14d79e5a6e143766f87fc25
idudiq <idudiq@gmail.com>
2019-08-12 14:37:20 +0300

    test(e2e): fix unlink call

--- scripts/build-connect.js
+++ scripts/build-connect.js
@@ -1,8 +1,12 @@
+const childProcess = require('child_process');
 const executor = require('./executor');
 
 // build connect
 try {
-  executor('yarn unlink');
+  childProcess.execSync('yarn unlink', { stdio: 'inherit' });
 } catch (e) {}
-executor('yarn build');
-executor('yarn link');
+executor([
+  //
+  'yarn build',
+  'yarn link',
+]);
