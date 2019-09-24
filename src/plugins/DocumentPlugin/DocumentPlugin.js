5828c43d8aae13282f652077b41dbd93f3194a23
idudiq <idudiq@gmail.com>
2019-09-24 11:55:02 +0300

    refactor: change create document flow

--- src/plugins/DocumentPlugin/DocumentPlugin.js
+++ src/plugins/DocumentPlugin/DocumentPlugin.js
@@ -1,8 +1,11 @@
+import ConnectError from '@endpass/class/ConnectError';
 import PluginBase from '../PluginBase';
 import { DialogPlugin } from '@/plugins/DialogPlugin';
 import { MessengerGroupPlugin } from '@/plugins/MessengerGroupPlugin';
 import DocumentPublicApi from './DocumentPublicApi';
 import { MESSENGER_METHODS, PLUGIN_NAMES } from '@/constants';
+
+const { ERRORS } = ConnectError;
 
 export default class DocumentPlugin extends PluginBase {
   static get pluginName() {
@@ -17,7 +20,19 @@ export default class DocumentPlugin extends PluginBase {
     return DocumentPublicApi;
   }
 
+  /**
+   *
+   * @return {Promise<object>}
+   */
   async createDocument() {
-    return this.context.ask(MESSENGER_METHODS.CREATE_DOCUMENT);
+    const { status, code, payload = {} } = await this.context.ask(
+      MESSENGER_METHODS.CREATE_DOCUMENT,
+    );
+
+    if (!status) {
+      throw ConnectError.create(code || ERRORS.CREATE_DOCUMENT);
+    }
+
+    return payload;
   }
 }
---
+++ tests/unit/spec/plugins/DocumentPlugin/DocumentPlugin.spec.js
@@ -0,0 +1,74 @@
+import { DocumentPlugin } from '@/plugins/DocumentPlugin';
+import { MESSENGER_METHODS } from '@/constants';
+
+describe('Document plugin', () => {
+  let plugin;
+  const options = {};
+  const docId = 'docId';
+
+  const context = {
+    ask: jest.fn(),
+    executeMethod: jest.fn(),
+    isLogin: true,
+  };
+
+  const createPlugin = () => {
+    const inst = new DocumentPlugin(options, context);
+    inst.init();
+    return inst;
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    plugin = createPlugin();
+  });
+
+  describe('initial', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('should create instance of plugin', () => {
+      expect(plugin).toBeInstanceOf(DocumentPlugin);
+    });
+  });
+
+  describe('createDocument', () => {
+    it('should create document and return id', async () => {
+      expect.assertions(2);
+
+      context.ask = jest.fn().mockResolvedValueOnce({
+        status: true,
+        payload: {
+          id: docId,
+        },
+      });
+
+      const res = await plugin.createDocument();
+
+      expect(res).toEqual({
+        id: docId,
+      });
+
+      expect(context.ask)
+        .toBeCalledWith(MESSENGER_METHODS.CREATE_DOCUMENT);
+    });
+
+    it('should throw error for create document', async () => {
+      expect.assertions(2);
+
+      context.ask = jest.fn().mockResolvedValueOnce({
+        status: false,
+      });
+
+      try {
+        await plugin.createDocument();
+      } catch (e) {
+        expect(e).toEqual(new Error('Document creation error'));
+      }
+
+      expect(context.ask)
+        .toBeCalledWith(MESSENGER_METHODS.CREATE_DOCUMENT);
+    });
+  });
+});
