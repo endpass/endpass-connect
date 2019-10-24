ca8f5af57a69d0955562a49fc0957f77bcae2d16
idudiq <idudiq@gmail.com>
2019-10-24 08:03:06 +0300

    test(e2e): order of check routes

--- tests/e2e/specs/login.spec.js
+++ tests/e2e/specs/login.spec.js
@@ -119,10 +119,9 @@ describe('login', function() {
       cy.authFrameContinueRun();
 
       cy.wait('@routeAuthCheck');
+      cy.wait('@routeAuthCheck');
 
       cy.get('[data-test=endpass-app-loader]').should('exist');
-
-      cy.wait('@routeAuthCheck');
 
       cy.getElementFromAuth('[data-test=modal-card-button-close]').click();
 
