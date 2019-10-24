0634a2684850c9ec479abf37e91a4920a2db91ea
idudiq <idudiq@gmail.com>
2019-10-24 16:36:48 +0300

    test(e2e): switch to chrome from electron cli

--- scripts/e2e-run.js
+++ scripts/e2e-run.js
@@ -1,5 +1,5 @@
 const executor = require('./executor');
 require('./e2e-serve.js');
 
-executor('cypress run');
+executor('cypress run --browser chrome');
 executor.exit();
--- tests/e2e/specs/login.spec.js
+++ tests/e2e/specs/login.spec.js
@@ -1,6 +1,6 @@
 import { email, v3password, mnemonic, regularPassword, otpCode } from '@fixtures/identity/accounts';
 
-describe.skip('login', function() {
+describe('login', function() {
   describe('connect login features', () => {
     beforeEach(() => {
       cy.waitPageLoad();
