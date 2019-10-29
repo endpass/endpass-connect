0127628f3b426ed7421fa5794f076897efc8cd38
Danil Kosterin <raymondcostner@gmail.com>
2019-10-29 13:19:00 +0300

    fix: remove types from `globals.d.ts`

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -5,20 +5,6 @@ declare namespace ENV {
 
 declare type Listener = (...args: any) => void
 
-declare module '@endpass/utils/mapToQueryString' {
-  function mapToQueryString(url: string, params: object): string;
-  export = mapToQueryString;
-}
-
-declare module '@endpass/utils/queryStringToMap' {
-  function queryStringToMap(path: string): object;
-  export = queryStringToMap;
-}
-
-declare module '@endpass/utils/numbers' {
-  function isNumeric(number: string | number): boolean;
-}
-
 declare module '@endpass/class/LocalStorage' {
   function remove(key: string): void;
   function save(key: string, data: any): void;
