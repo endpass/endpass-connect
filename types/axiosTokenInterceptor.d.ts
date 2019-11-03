7f87f85cb9545d0f72ab4fdddc8d390cb7e92795
Danil Kosterin <raymondcostner@gmail.com>
2019-11-03 20:51:07 +0300

    feat: describe types for `axios-token-interceptor`

---
+++ types/axiosTokenInterceptor.d.ts
@@ -0,0 +1,18 @@
+declare module "axios-token-interceptor" {
+  import {AxiosRequestConfig} from 'axios';
+
+  type InterceptorOptions = {
+    header?: string,
+    headerFormatter?: Function,
+    getToken?: any,
+    token?: object | Promise<any>,
+    getMaxAge?: Function,
+    maxAge?: number,
+  };
+
+  type GetTokenFunction = (options: InterceptorOptions) => string;
+  type InterceptRequestFunction = (value: AxiosRequestConfig) => AxiosRequestConfig | Promise<AxiosRequestConfig>;
+
+  export default function tokenInterceptor(options: InterceptorOptions): InterceptRequestFunction;
+  export function tokenCache(getToken: GetTokenFunction, options: InterceptorOptions): any;
+}
