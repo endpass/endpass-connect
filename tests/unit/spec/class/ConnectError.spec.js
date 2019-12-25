fd41bb46eb6573a552f624fcbaf046304d42792b
idudiq <idudiq@gmail.com>
2019-12-25 13:57:29 +0300

    fix: process connect errors from iframe

--- tests/unit/spec/class/ConnectError.spec.js
+++ tests/unit/spec/class/ConnectError.spec.js
@@ -41,7 +41,7 @@ describe('ConnectError', () => {
   });
 
   it('should pass already created error with default code', () => {
-    const checkError = new ConnectError(customMessage);
+    const checkError = new ConnectError(customMessage, ERRORS.AUTH);
 
     const err = ConnectError.createFromError(
       checkError,
@@ -49,7 +49,7 @@ describe('ConnectError', () => {
     );
 
     expect(err).toBe(checkError);
-    expect(err.code).toBe(ERRORS.NOT_DEFINED);
+    expect(err.code).toBe(ERRORS.AUTH);
   });
 
   it('should rewrite error to ConnectError', () => {
@@ -77,5 +77,40 @@ describe('ConnectError', () => {
     expect(err.code).toBe(ERRORS.AUTH_CANCELED_BY_USER);
     expect(err.code).toBe(checkError.code);
     expect(err.message).toBe(checkError.message);
+  });
+
+  it('should create error by other ConnectError', () => {
+    const code = 'code';
+    const message = 'message';
+    const obj = {
+      code,
+      message,
+      constructor: {
+        name: 'ConnectError',
+      },
+    };
+    const err = ConnectError.createFromError(obj);
+
+    expect(err).toBeInstanceOf(ConnectError);
+    expect(err.code).toBe(code);
+    expect(err.message).toBe(message);
+  });
+
+  it('should create default error if not passed data', () => {
+    const err = ConnectError.createFromError();
+
+    expect(err).toBeInstanceOf(ConnectError);
+    expect(err.code).toBe(ERRORS.NOT_DEFINED);
+    expect(err.message).toBe('Endpass connect not defined error');
+  });
+
+  it('should create default error by code if error wrong', () => {
+    const code = 'code';
+    const message = 'message';
+    const err = ConnectError.createFromError({ code, message });
+
+    expect(err).toBeInstanceOf(ConnectError);
+    expect(err.code).toBe(ERRORS.NOT_DEFINED);
+    expect(err.message).toBe('Endpass connect not defined error');
   });
 });
