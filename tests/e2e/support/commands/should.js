c482584349957585ef2154c5389979d7b70f03a3
idudiq <idudiq@gmail.com>
2019-12-27 19:15:29 +0300

    test(e2e): updated submodules

--- tests/e2e/support/commands/should.js
+++ tests/e2e/support/commands/should.js
@@ -4,7 +4,11 @@ Cypress.Commands.add('shouldLoggedIn', () => {
   cy.wait('@balance', {
     timeout: 20000,
   });
+
+  // error is here happens
+  // <div.v-spinner.is-overlay> is linked with [data-test=endpass-app-loader]
   cy.get('[data-test=endpass-app-loader]').should('not.exist');
+
   cy.authFrameWrapperHidden().should('exist');
   cy.getElementFromWidget('[data-test=widget-header]').should('exist');
   cy.get('[data-test=endpass-form-basic-active-account]').contains(address);
