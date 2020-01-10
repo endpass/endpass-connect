6931dbce80134bbb38ba0359e27e02a29eaad80e
idudiq <idudiq@gmail.com>
2020-01-10 11:07:03 +0300

    fix(e2e): add headless flag

--- scripts/e2e-run.js
+++ scripts/e2e-run.js
@@ -1,5 +1,5 @@
 const executor = require('./executor');
 require('./e2e-serve.js');
 
-executor('cypress run --browser chrome');
+executor('cypress run --headless --browser chrome');
 executor.exit();
