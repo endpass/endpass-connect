c87336d1c87fd04dae30629efbe011b8e9c6e6ce
idudiq <idudiq@gmail.com>
2020-01-10 15:38:47 +0300

    fix: add logger for e2e tests

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -1,5 +1,6 @@
 const webpack = require('@cypress/webpack-preprocessor');
 const path = require('path');
+const logOutput = require('cypress-log-to-output');
 
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -14,9 +15,11 @@ const path = require('path');
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = (on, config) => {
+module.exports = on => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  logOutput.install(on);
 
   const options = {
     webpackOptions: {
