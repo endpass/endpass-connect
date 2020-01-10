2e6fead41580510813ef664b2a67cb83ea92f04a
idudiq <idudiq@gmail.com>
2020-01-10 16:46:32 +0300

    fix(e2e): disable headless mode

--- scripts/e2e-run.js
+++ scripts/e2e-run.js
@@ -1,5 +1,6 @@
 const executor = require('./executor');
 require('./e2e-serve.js');
 
-executor('cypress run --headless --browser chrome');
+executor('cypress run --browser chrome');
+// executor('cypress run --headless --browser chrome');
 executor.exit();
