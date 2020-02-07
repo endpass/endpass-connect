1281e66aeff20cabbefb0c3e61af6bec81f5a069
idudiq <idudiq@gmail.com>
2020-02-07 10:27:51 +0300

    fix: we check

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -15,6 +15,8 @@ const browserSize = require('./browserSize');
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
