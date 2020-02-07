da1bcb88702c42cc2b39daeb0a063c4d51ce6e75
idudiq <idudiq@gmail.com>
2020-02-07 15:46:48 +0300

    test(e2e): update cy version

--- scripts/e2e-run.js
+++ scripts/e2e-run.js
@@ -2,5 +2,5 @@ const executor = require('./executor');
 require('./e2e-serve.js');
 
 // --headless flag is not working correctly with cypress.viewport(); define
-executor('cypress run --browser --headless chrome');
+executor('cypress run --browser chrome');
 executor.exit();
